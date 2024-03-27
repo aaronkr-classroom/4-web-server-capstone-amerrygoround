c3310e607d151901f71f4de65b4cbe21bec6b1be
amerrygoround <163214901+amerrygoround@users.noreply.github.com>
2024-03-27 02:40:17 +0000

    수정요망

---
+++ content-types.js
@@ -0,0 +1,25 @@
+// content-types.js
+"use strict";
+
+// Listening 7.4 (p. 112)
+module.exports = {
+    html: {
+        "Content-Type": "text/plain"
+    },
+    text:{
+        "Content-Type": "text/text"
+    },
+    css:{
+        "Content-Type": "text/css"
+    },
+    js: {
+        "Content-Type": "text/javascript"
+    },
+    jpg: {
+
+    },
+    png: {
+        
+    }
+
+}
